e35173b47a75b4fe269d27fa748d2661c4be9de5
jarol1590 <jarol1590@gmail.com>
2024-03-07 12:33:10 -0500

    Se crean SVG para información de Nodos e Informacion de Arcos, se crea el submenú de Nuevo Grafo y se agregan sus funcionalidades

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule, SidemenuComponent],
+  imports: [RouterModule, SidemenuComponent, CommonModule],
   templateUrl: './inicio.component.html',
   styles: ``
 })
 export default class InicioComponent {
+  openSubMenu: boolean = false;
+  isNuevoGrafoSelected: boolean = false;
+
+  openSubSubMenu(event: MouseEvent) {
+    event.stopPropagation(); // Evita que el evento se propague a elementos padre
+    // Agrega aquí la lógica para abrir el segundo submenú
+  }
+
+
+  toggleSubMenu(event: MouseEvent): void {
+    this.openSubMenu = !this.openSubMenu;
+    event.stopPropagation(); // Para evitar que el clic se propague al hacer clic dentro del submenú
+  }
+
+  closeSubMenu(): void {
+    this.openSubMenu = false;
+  }
+
+  toggleNuevoGrafoSubMenu(event: MouseEvent): void {
+    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    this.isNuevoGrafoSelected = !this.isNuevoGrafoSelected;
+    event.stopPropagation(); // Para evitar que el clic se propague al hacer clic dentro del submenú
+}
+
+
+
+// si se da click en otro lugar cierra el submenu
+  @HostListener('document:click', ['$event'])
+  handleOutsideClick(event: MouseEvent) {
+    if (!(event.target as HTMLElement).closest('.relative')) {
+      this.closeSubMenu();
+    }
+  }
 
 }
