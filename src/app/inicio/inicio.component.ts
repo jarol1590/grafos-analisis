3e5bd380f89b1701f4b983f880ac4edff889c29b
jarol1590 <jarol1590@gmail.com>
2024-03-07 20:41:02 -0500

    configuracion de submenu de Exportar Datos e Importar Datos

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -12,36 +12,70 @@ import { CommonModule } from '@angular/common';
 })
 export default class InicioComponent {
   openSubMenu: boolean = false;
+  openSubSubMenu: boolean = false;
   isNuevoGrafoSelected: boolean = false;
+  isExportarDatosSelected: boolean =false;
+  isImportarDatosSelected: boolean =false;
 
-  openSubSubMenu(event: MouseEvent) {
-    event.stopPropagation(); // Evita que el evento se propague a elementos padre
-    // Agrega aquí la lógica para abrir el segundo submenú
-  }
+  
 
 
   toggleSubMenu(event: MouseEvent): void {
     this.openSubMenu = !this.openSubMenu;
     event.stopPropagation(); // Para evitar que el clic se propague al hacer clic dentro del submenú
   }
+  toggleSubSubMenu(event: MouseEvent): void{
+    this.openSubSubMenu = !this.openSubSubMenu;
+    event.stopPropagation();
+  }
 
   closeSubMenu(): void {
     this.openSubMenu = false;
   }
 
+  closeSubSubMenu():void {
+    this.openSubSubMenu = false;
+  }
+
   toggleNuevoGrafoSubMenu(event: MouseEvent): void {
-    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
     this.isNuevoGrafoSelected = !this.isNuevoGrafoSelected;
     event.stopPropagation(); // Para evitar que el clic se propague al hacer clic dentro del submenú
-}
+  }
 
+  toggleEImportarDatosSubMenu(event: MouseEvent): void{
+    this.isImportarDatosSelected = !this.isImportarDatosSelected;
+    event.stopPropagation();
 
+  }
 
-// si se da click en otro lugar cierra el submenu
+  closeNuevoGrafoSubMenu(): void{
+    this.isNuevoGrafoSelected = false;
+  }
+
+  toggleExportarDatosSubMenu(event: MouseEvent): void {
+    this.isExportarDatosSelected= !this.isExportarDatosSelected;
+    event.stopPropagation();
+  }
+
+  closeExportarDatosSubMenu():void{
+    this.isExportarDatosSelected = false;
+  }
+
+  closeImportarDatosSubMenu():void{
+    this.isImportarDatosSelected = false;
+  }
+
+ 
+
+  // si se da click en otro lugar cierra el submenu
   @HostListener('document:click', ['$event'])
   handleOutsideClick(event: MouseEvent) {
     if (!(event.target as HTMLElement).closest('.relative')) {
       this.closeSubMenu();
+      this.closeSubSubMenu();
+      this.closeNuevoGrafoSubMenu();
+      this.closeExportarDatosSubMenu();
+      this.closeImportarDatosSubMenu();
     }
   }
 
